dce334c5314fb4b77090c8201c7bf683183d9379
shaukathussain <155580193+sherijigarx@users.noreply.github.com>
2024-02-02 13:44:30 +0100

    Update tts.py

--- classes/tts.py
+++ classes/tts.py
@@ -107,7 +107,7 @@ class TextToSpeechService(AIModelService):
                 if step % 50 == 0:
                     lib.utils.try_update()
 
-                if step % 10:  # Assuming each loop is ~0.5 seconds, adjust as needed
+                if step % 3600:  # Assuming each loop is ~0.5 seconds, adjust as needed
                     self.outdated_miners_set = await self.filtered_UIDs()
                     bt.logging.info(f"Outdated miners before going to exclude_outdated_miners variable: {self.outdated_miners_set}")
             except KeyboardInterrupt:
