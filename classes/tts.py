120da8d5446b495f9463a1bee7f07b8991d27418
isharab <sherijigarx@gmail.com>
2024-02-02 23:35:39 +0500

    Update tts.py

--- classes/tts.py
+++ classes/tts.py
@@ -183,7 +183,7 @@ class TextToSpeechService(AIModelService):
     
     def update_block(self):
         self.current_block = self.subtensor.block
-        if self.current_block - self.last_updated_block > 100:
+        if self.current_block - self.last_updated_block > 20:
             bt.logging.info(f"Updating weights. Last update was at block {self.last_updated_block}")
             bt.logging.info(f"Current block is {self.current_block}")
             if self.uid not in self.runs_data_valid:
@@ -194,7 +194,7 @@ class TextToSpeechService(AIModelService):
         else:
             bt.logging.info(f"Updating weights. Last update was at block:  {self.last_updated_block}")
             bt.logging.info(f"Current block is: {self.current_block}")
-            bt.logging.info(f"Next update will be at block: {self.last_updated_block + 100}")
+            bt.logging.info(f"Next update will be at block: {self.last_updated_block + 20}")
             bt.logging.info(f"Skipping weight update. Last update was at block {self.last_updated_block}")
 
     def process_responses(self,filtered_axons, responses, prompt):
