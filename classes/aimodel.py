f884023675ae20aeec21a54db26988083629f762
shaukathussain <155580193+sherijigarx@users.noreply.github.com>
2024-02-03 05:58:23 +0100

    Update aimodel.py

--- classes/aimodel.py
+++ classes/aimodel.py
@@ -251,10 +251,12 @@ class AIModelService:
                 self.runs_data_valid = list(set(self.runs_data_valid))
 
 
+
+
     async def filtered_UIDs_Miner(self):
         owner = "UncleTensor"  # Replace with actual GitHub owner
         repo = "AudioSubnet"    # Replace with actual GitHub repository
-        bt.logging.info(f"..........................fetch Processing running here Filtred UID in 209....................................")
+        bt.logging.info("..........................fetch Processing running here Filtred UID in 209....................................")
 
         # Get the latest commit SHA
         latest_commit = await self.get_latest_commit(owner, repo)
@@ -276,18 +278,19 @@ class AIModelService:
                 if file.name == 'wandb-metadata.json':
                     await file.download(root=self.download_dir, replace=True)
                     file_path = os.path.join(self.download_dir, file.name)
-                    with open(file_path, 'r') as f:
-                        metadata = json.load(f)
+                    async with aiofiles.open(file_path, 'r') as f:
+                        metadata = json.loads(await f.read())
                         if 'git' in metadata:
                             run_data['Git Commit'] = metadata['git']['commit']
 
             # Filter out runs not having the latest commit hash
             if run_data['Git Commit'] == latest_commit:
-                await self.runs_data.append(run_data['UID'])
+                self.runs_data.append(run_data['UID'])  # Note: Removed await here as append is not an async operation
                 bt.logging.info(f"Run data.........................................: {run_data['UID']}")
                 bt.logging.info(f"Run data.........................................: {run_data['Hotkey']}")
                 bt.logging.info(f"Run data.........................................: {run_data['Git Commit']}")
-                self.runs_data = list(set(self.runs_data_valid))
+        self.runs_data = list(set(self.runs_data))  # Note: Corrected to set(self.runs_data) to deduplicate
+
 
 
 
